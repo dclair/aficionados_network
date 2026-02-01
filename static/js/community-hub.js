b3ece16660d6beec3dcf2dfe237fe83209230aa7
Dclair <0y1informatica@gmail.com>
2026-02-01 15:36:47 +0000

    Antes de empezar a la confimación de la cuenta por email. Finalizar el registro confirmando con link desde el email.

--- static/js/community-hub.js
+++ static/js/community-hub.js
@@ -103,3 +103,20 @@ document.addEventListener('DOMContentLoaded', () => {
     CommunityHub.initSmartScroll();
     CommunityHub.initLoadingState(); // Se encarga de todo ahora
 });
+
+
+// Botón Back to Top
+const mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        mybutton.classList.remove("d-none");
+    } else {
+        mybutton.classList.add("d-none");
+    }
+};
+
+mybutton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+   
